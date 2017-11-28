8b3b664fb54c8b070927d88b4efac2d92c0600dd
Kevin Zhao <kevinzha@microsoft.com>
2017-11-28 14:45:33 +0800

    notify server at Client connected and disconnected

--- src/Microsoft.AspNetCore.SignalR.ServiceServer/HubMessageBroker.cs
+++ src/Microsoft.AspNetCore.SignalR.ServiceServer/HubMessageBroker.cs
@@ -13,6 +13,10 @@ namespace Microsoft.AspNetCore.SignalR
         private const string ConnectionIdKeyName = "connId";
         private const string GroupNameKeyName = "groupName";
         private const string ExcludedIdsKeyName = "excluded";
+
+        private const string OnConnectedMethodName = "OnConnectedAsync";
+        private const string OnDisconnectedMethodName = "OnDisconnectedAsync";
+
         private readonly ConcurrentDictionary<string, HubLifetimeManager<ClientHub>> _clientHubManagerDict = new ConcurrentDictionary<string, HubLifetimeManager<ClientHub>>();
         private readonly ConcurrentDictionary<string, HubLifetimeManager<ServerHub>> _serverHubManagerDict = new ConcurrentDictionary<string, HubLifetimeManager<ServerHub>>();
         private readonly ConcurrentDictionary<string, string> _pendingClientCalls = new ConcurrentDictionary<string, string>();
@@ -23,6 +27,9 @@ namespace Microsoft.AspNetCore.SignalR
         {
             var clientHubManager = _clientHubManagerDict.GetOrAdd(hubName, new DefaultHubLifetimeManager<ClientHub>());
             await clientHubManager.OnConnectedAsync(context);
+            // Invoke OnConnectedAsync on server
+            await PassThruClientMessage(hubName, context,
+                new InvocationMessage(Guid.NewGuid().ToString(), true, OnConnectedMethodName, null, null));
         }
 
         public async Task OnClientDisconnectedAsync(string hubName, HubConnectionContext2 context)
@@ -31,6 +38,9 @@ namespace Microsoft.AspNetCore.SignalR
             if (clientHubManager != null)
             {
                 await clientHubManager.OnDisconnectedAsync(context);
+                // Invoke OnDiconnectedAsync on server
+                await PassThruClientMessage(hubName, context,
+                    new InvocationMessage(Guid.NewGuid().ToString(), true, OnDisconnectedMethodName, null, null));
             }
         }
 
@@ -39,7 +49,12 @@ namespace Microsoft.AspNetCore.SignalR
             var serverHubManager = _serverHubManagerDict[hubName];
             if (serverHubManager != null)
             {
-                _pendingClientCalls.TryAdd(message.InvocationId, context.ConnectionId);
+                if (!string.Equals(message.Target, OnConnectedMethodName, StringComparison.InvariantCultureIgnoreCase) &&
+                    !string.Equals(message.Target, OnDisconnectedMethodName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _pendingClientCalls.TryAdd(message.InvocationId, context.ConnectionId);
+                }
+
                 // Add original connection Id to message metadata
                 message.Metadata.Add(ConnectionIdKeyName, context.ConnectionId);
 
