182fae6617a3f9293b599ce32af6e06fc9cb467a
Kevin Zhao <kevinzha@microsoft.com>
2017-11-28 19:18:45 +0800

    add initial implementation of connection router

---
+++ src/Microsoft.AspNetCore.SignalR.ServiceServer/IHubConnectionRouter.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.AspNetCore.SignalR.ServiceServer
+{
+    public interface IHubConnectionRouter
+    {
+        void OnClientConnected(string hubName, HubConnectionContext connection);
+        void OnClientDisconnected(string hubName, HubConnectionContext connection);
+        void OnServerConnected(string hubName, HubConnectionContext connection);
+        void OnServerDisconnected(string hubName, HubConnectionContext connection);
+    }
+}
